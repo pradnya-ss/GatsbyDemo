2fa456d7ee3ae17e757c25776086a878a7c86381
shreyas-1997 <66195578+shreyas-1997@users.noreply.github.com>
2020-08-26 12:37:41 +0530

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost/Allaboutvat/graphql',
+    uri: 'http://15.207.185.188/graphql',
     fetch,
   }),
   cache: new InMemoryCache()
